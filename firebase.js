b97c78330c0d1402120253bc505084fa19c78b84
Min Khaung <minkhaung.mkks@gmail.com>
2023-08-26 23:13:02 +0630

    Hopefully final bug fix

--- firebase.js
+++ firebase.js
@@ -24,6 +24,7 @@ const fetchData = async () => {
         console.error('Fetch error:', error);
     }
 };
+let userAgenda;
 
 const startApp = async () => {
     const firebaseConfig = {
@@ -33,12 +34,29 @@ const startApp = async () => {
     console.log(firebaseConfig)
     const app = initializeApp(firebaseConfig);
     const database = getDatabase(app)
+    userAgenda = ref(database, "To-Dos")
+    onValue(userAgenda, (snapshot) => {
+        reset()
+        const data = Object.entries(snapshot.val());
+        for (let i = 0; i < data.length; i++) {
+            let currentData = data[i]
+            const id = currentData[0]
+            const text = currentData[1]
+            let newCard = CreateAgendaCard(text)
+            newCard.addEventListener('click', () => {
+                console.log(id)
+                let itemLocationInDb = ref(database, `To-Dos/${id}`)
+                remove(itemLocationInDb)
+            })
+            agendaBox.append(newCard)
+        }
+    })
 }
 startApp()
 
 
 const agendaBox = document.querySelector('.agenda_box')
-const userAgenda = ref(database, "To-Dos")
+
 const inputField = document.getElementById('new_item_input')
 
 export const AddNewToDo = () => {
@@ -48,22 +66,7 @@ export const AddNewToDo = () => {
     console.log(`${inputValue} added to the database`)
 }
 
-onValue(userAgenda, (snapshot) => {
-    reset()
-    const data = Object.entries(snapshot.val());
-    for (let i = 0; i < data.length; i++) {
-        let currentData = data[i]
-        const id = currentData[0]
-        const text = currentData[1]
-        let newCard = CreateAgendaCard(text)
-        newCard.addEventListener('click', () => {
-            console.log(id)
-            let itemLocationInDb = ref(database, `To-Dos/${id}`)
-            remove(itemLocationInDb)
-        })
-        agendaBox.append(newCard)
-    }
-})
+
 
 const reset = () => {
     const agendaCards = document.querySelectorAll('.agenda_card')
